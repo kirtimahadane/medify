6fc5c200c32ee76149ea93c328139144b7d5cc8f
kirtimahadane <kirti.mahadane@gmail.com>
2024-04-01 10:46:40 +0530

    react bootstrsp and font poppins

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const App = () => {
   return (
-    <div>App</div>
+  <Container className='text-center'>
+    <Row>
+      <Col sm={6}>App One
+      </Col>
+      <Col sm={6}>App Two
+      </Col>
+    </Row>
+    </Container>
   )
 }
 
